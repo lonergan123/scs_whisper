c99e232b660e784ad3d60cfabe52d85108d586a2
Kevin Lonergan <lonergan123@hotmail.com>
2024-02-03 12:21:54 -0700

    Add logging functionality to main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import logging
 import numpy as np
 from fastapi import FastAPI, Request
 import whisper 
+
+# Logging
+def get_logger(logger_name):
+   logger = logging.getLogger(logger_name)
+   logger.setLevel(logging.DEBUG)
+   handler = logging.StreamHandler(sys.stdout)
+   handler.setLevel(logging.DEBUG)
+   handler.setFormatter(
+      logging.Formatter(
+      '%(name)s [%(asctime)s] [%(levelname)s] %(message)s'))
+   logger.addHandler(handler)
+   return logger
+logger = get_logger('snowpark-container-service')
 
 app = FastAPI()
 
@@ -27,5 +41,5 @@ async def readstage():
 @app.get("/transcripe_stage_audio")
 async def transcripe_stage_audio():  
     model = whisper.load_model("tiny.en", download_root="/whisper_models")
-    result = model.transcribe("/audio_files/SampleMedDictation.mp3")
+    result = model.transcribe(whisper.load_audio("/audio_files/SampleMedDictation.mp3"))
     return result["text"]
