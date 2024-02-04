43bec56fe72e5f7dbe528209fba80725a3bd1aa5
Kevin Lonergan <lonergan123@hotmail.com>
2024-02-03 23:21:08 -0700

    Refactor audio loading function in core.py

--- openai_whisper/core.py
+++ openai_whisper/core.py
@@ -31,39 +31,6 @@ else:
     model = whisper.load_model(model_name, download_root='/whisper_models')
 model_lock = Lock()
 
-def load_audio(file: str, encode=True, sr: int = 16000):
-    """
-    Open an audio file object and read as mono waveform, resampling as necessary.
-    Parameters
-    ----------
-    encode: Boolean
-        If true, encode audio stream to WAV before sending to whisper
-    sr: int
-        The sample rate to resample the audio if necessary
-    Returns
-    -------
-    A NumPy array containing the audio waveform, in float32 dtype.
-    """
-  
-    if encode:
-        try:
-            # ffmpeg to read audio data from requests
-            cmd = [
-                "ffmpeg",
-                "-nostdin",
-                "-threads", "0",
-                "-i", file,
-                "-f", "s16le",
-                "-ac", "1",
-                "-acodec", "pcm_s16le",
-                "-ar", str(16000),
-                "-"
-            ]
-            out = run(cmd, capture_output=True, check=True).stdout
-        except Exception as e:
-            raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
-
-    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
 
 def transcribe(audio_file_path: str):
     with model_lock:
